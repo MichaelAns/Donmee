618dd195994d5319ef7bd087847a4e9e690fc202
MichaelAns <mi035@mail.ru>
2023-03-27 00:03:48 +0300

    Created the IDonmeeDbContext

---
+++ Donmee.Frontend/src/Frontend.Persistance/IDonmeeDbContext.cs
@@ -0,0 +1,9 @@
+﻿namespace Frontend.Persistance
+{
+    internal class IDonmeeDbContext
+    {
+        public DbSet<User> User { get; set; }
+        public DbSet<Wish> Wish { get; set; }
+        public DbSet<Transaction> Transaction { get; set; }
+    }
+}
