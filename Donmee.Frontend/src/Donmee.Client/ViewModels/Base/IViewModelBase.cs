9a5fcf63f7065ab9fd5db7b86448096c4b20e38e
MichaelAns <mi035@mail.ru>
2023-03-31 01:04:17 +0300

    Created the IViewModelBase and ViewModelBase

---
+++ Donmee.Frontend/src/Donmee.Client/ViewModels/Base/IViewModelBase.cs
@@ -0,0 +1,15 @@
+﻿using Donmee.Client.Services.Navigation;
+
+namespace Donmee.Client.ViewModels.Base
+{
+    public interface IViewModelBase
+    {
+        public INavigationService NavigationService { get; }
+
+        public bool IsBusy { get; }
+
+        public bool IsInitialized { get; set; }
+
+        Task InitializeAsync();
+    }
+}
---
+++ Donmee.Frontend/src/Donmee.Client/ViewModels/Base/ViewModelBase.cs
@@ -0,0 +1,70 @@
+﻿using Donmee.Client.Services.Navigation;
+
+namespace Donmee.Client.ViewModels.Base
+{
+    public abstract partial class ViewModelBase : ObservableObject, IViewModelBase, IDisposable
+    {
+        public ViewModelBase(INavigationService navigationService)
+        {
+            NavigationService = navigationService;
+        }
+
+        private readonly SemaphoreSlim _isBusyLock = new(1, 1);
+
+        private bool _disposedValue;
+
+        [ObservableProperty]
+        private bool _isInitialized;
+
+        [ObservableProperty]
+        private bool _isBusy;
+
+        public INavigationService NavigationService { get; private set; }        
+
+        public virtual void ApplyQueryAttributes(IDictionary<string, object> query)
+        {
+        }
+
+        public virtual Task InitializeAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public async Task IsBusyFor(Func<Task> unitOfWork)
+        {
+            await _isBusyLock.WaitAsync();
+
+            try
+            {
+                IsBusy = true;
+
+                await unitOfWork();
+            }
+            finally
+            {
+                IsBusy = false;
+                _isBusyLock.Release();
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposedValue)
+            {
+                if (disposing)
+                {
+                    _isBusyLock?.Dispose();
+                }
+
+                _disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
