5eb825654fe7474ac08bbe66198a6100c4ba6010
MichaelAns <mi035@mail.ru>
2023-03-31 00:34:20 +0300

    Routed the un-Tabbar views

--- Donmee.Frontend/src/Donmee.Client/AppShell.xaml.cs
+++ Donmee.Frontend/src/Donmee.Client/AppShell.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Donmee.Client.Services.Navigation;
+using Donmee.Client.Views;
 
 namespace Donmee.Client;
 
@@ -7,6 +8,8 @@ public partial class AppShell : Shell
     public AppShell(INavigationService navigationService)
     {
         _navigationService = navigationService;
+
+        InitializeRouting();
 
         InitializeComponent();
     }
@@ -20,6 +23,16 @@ public partial class AppShell : Shell
         {
             await _navigationService.InitializeAsync();
         }
+    }
+
+    // Register the un-TabBar routes
+    private static void InitializeRouting()
+    {
+        Routing.RegisterRoute("Login", typeof(LoginView));
+        Routing.RegisterRoute("SignUp", typeof(SignUpView));
+        Routing.RegisterRoute("WishDetails", typeof(WishDetailsView));
+        Routing.RegisterRoute("MyWishDetails", typeof(MyWishDetailsView));
+        Routing.RegisterRoute("SignUp", typeof(SignUpView));
     }
 
 
