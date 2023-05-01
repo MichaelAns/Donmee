f98deca4bd2abc12d37edccd43a1b3a8faca0f0a
MichaelAns <mi035@mail.ru>
2023-05-01 03:24:12 +0300

    Created the AuthController

--- Donmee.Backend/src/Donmee.Persistence/Models/User.cs
+++ Donmee.Backend/src/Donmee.Persistence/Models/User.cs
@@ -2,13 +2,13 @@
 {
     public class User
     {
-        public Guid Id { get; set; }
+        public Guid Id { get; set; } = new Guid();
         public string Name { get; set; }
         public string SecondName { get; set; }
         public string Phone { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
-        public double Balance { get; set; }
+        public double Balance { get; set; } = 0;
         // public double Bonus { get; set; }
 
         public virtual IEnumerable<Transaction>? Transactions { get; set; }
--- Donmee.Backend/src/Donmee.WebApi/Controllers/AuthController.cs
+++ Donmee.Backend/src/Donmee.WebApi/Controllers/AuthController.cs
@@ -1,11 +1,85 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Donmee.Persistence.Models;
+using Donmee.WebApi.Configurations;
+using Donmee.WebApi.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
 
 namespace Donmee.WebApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]")] 
     [ApiController]
     public class AuthController : ControllerBase
     {
+        public AuthController(
+            UserManager<User> userManager, 
+            JwtConfig jwtConfig)
+        {
+            _userManager = userManager;
+            _jwtConfig = jwtConfig;
+        }
+        private readonly UserManager<Persistence.Models.User> _userManager;
+        private readonly JwtConfig _jwtConfig;
+
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register([FromBody] Domain.User requestUser)
+        {
+            // Validate the incoming request
+            if (ModelState.IsValid)
+            {
+                // Check the email existing
+                var userExist = await _userManager.FindByEmailAsync(requestUser.Email);
+
+                if (userExist != null)
+                {
+                    return BadRequest(new AuthResult()
+                    {
+                        Result = false,
+                        Errors = new List<string>()
+                        {
+                            "Email already exists"
+                        }
+                    });
+                }
+
+                // Create a user
+                string passwordHash = "password";
+
+                using (var hmac = new HMACSHA512())
+                {
+                    // TODO: encrypt
+                }
+
+                var newUser = new Persistence.Models.User()
+                {
+                    Name = requestUser.Name,
+                    SecondName = requestUser.SecondName,
+                    Email = requestUser.Email,
+                    Phone = requestUser.Phone,
+                    Password = passwordHash
+                };
+
+                var isCreated = await _userManager.CreateAsync(newUser, requestUser.Password);
+
+                if (isCreated.Succeeded)
+                {
+                    // Generate the token
+                }
+
+                return BadRequest(new AuthResult()
+                {
+                    Result = false,
+                    Errors = new List<string>()
+                        {
+                            "Server error"
+                        }
+                });
+            }
+
+            return BadRequest();
+        }
+
+        //
     }
 }
